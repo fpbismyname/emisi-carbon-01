45415fed7e28b63c3e8255aedee8b875089252b1
fpbismyname <fajarpebefajar12@gmail.com>
2024-12-09 16:44:26 +0700

    My Commit

--- app/controllers/LoginController.py
+++ app/controllers/LoginController.py
@@ -1,8 +1,9 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, jsonify
 LoginController = Blueprint("LoginController", __name__)
 
+# Make Methods Controller
 def index():
     return render_template("Auth/LoginPage.html")
 
 def login():
-    return "Logging in process..."
+    return jsonify(request.form.to_dict())
